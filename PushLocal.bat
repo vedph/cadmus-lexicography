8655781b7be7356f48d9bfe0df4635d948e10d81
Myrmex <dfusi@hotmail.com>
2025-10-11 16:23:01 +0200

    Update PushLocal.bat

--- PushLocal.bat
+++ PushLocal.bat
@@ -1,6 +1,7 @@
 @echo off
 echo PRESS ANY KEY TO INSTALL TO LOCAL NUGET FEED
 echo Remember to generate the up-to-date package.
-c:\exe\nuget add .\Cadmus.Geo.Parts\bin\Debug\Cadmus.Geo.Parts.4.0.6.nupkg -source C:\Projects\_NuGet
-c:\exe\nuget add .\Cadmus.Seed.Geo.Parts\bin\Debug\Cadmus.Seed.Geo.Parts.4.0.6.nupkg -source C:\Projects\_NuGet
+c:\exe\nuget add .\Cadmus.Lexicography.Parts\bin\Debug\Cadmus.Lexicography.Parts.0.0.1.nupkg -source C:\Projects\_NuGet
+c:\exe\nuget add .\Cadmus.Seed.Lexicography.Parts\bin\Debug\Cadmus.Seed.Lexicography.Parts.0.0.1.nupkg -source C:\Projects\_NuGet
+c:\exe\nuget add .\Cadmus.Lexicography.Services\bin\Debug\Cadmus.Lexicography.Services.0.0.1.nupkg -source C:\Projects\_NuGet
 pause
